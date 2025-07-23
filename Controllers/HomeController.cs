2954947c0bd3dd5479055d1d633c50a89ca54a5e
zezeyze <minozehra@hotmail.com>
2025-07-23 15:06:33 +0300

    Admin

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AdminBlog.Models;
+using AdminBlog.Models;
+
+namespace AdminBlog.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly BlogContext _context;
+
+
+
+    public HomeController(ILogger<HomeController> logger, BlogContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+    public async Task<IActionResult> AddCategory(Category category)
+    { 
+        await _context.AddAsync(category);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Category));
+    }
+    public IActionResult Category()
+    {
+        List<Category> list=_context.Category.ToList();
+        return View(list);
+    }
+    public async Task<IActionResult> DeleteCategory(int? Id)
+    {
+        Category category = await _context.Category.FindAsync(Id);
+        _context.Remove(category);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Category));
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using AdminBlog.Models;
+using Microsoft.AspNetCore.StaticAssets;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+
+var builder = WebApplication.CreateBuilder(args);
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BlogContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDb")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // Statik dosyalar için GEREKLÝ
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
