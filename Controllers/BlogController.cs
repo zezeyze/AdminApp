68d9c0a8428f0bc6eebdf8b9d585b0156f08ce7a
zezeyze <minozehra@hotmail.com>
2025-07-28 14:21:06 +0300

    Last update

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,18 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using AdminBlog.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.ComponentModel.Design;
 using System.IO;
-using System.Net;
-using System.Net.Cache;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
+using AdminBlog.Filter;
 
 namespace AdminBlog.Controllers
 {
+    [UserFilter]
     public class BlogController : Controller
     {
         private readonly ILogger<BlogController> _logger;
@@ -27,54 +28,47 @@ namespace AdminBlog.Controllers
         public IActionResult Index()
         {
             var list = _context.Blog.ToList();
+
             return View(list);
         }
         public IActionResult Publish(int Id)
         {
             var blog = _context.Blog.Find(Id);
-            blog.IsPublish=true;
+            blog.IsPublish = true;
             _context.Update(blog);
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
-
         public IActionResult Blog()
         {
-            // Kategorileri SelectListItem olarak hazýrlýyoruz
             ViewBag.Categories = _context.Category.Select(w =>
                 new SelectListItem
                 {
                     Text = w.Name,
                     Value = w.Id.ToString()
-                }).ToList();
-
+                }
+            ).ToList();
             return View();
         }
-
+        [HttpPost]
         public async Task<IActionResult> Save(Blog model)
         {
             if (model != null)
             {
                 var file = Request.Form.Files.First();
-
                 string savePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img");
-                Directory.CreateDirectory(savePath); // Klasör yoksa oluþtur
-
-                var extension = Path.GetExtension(file.FileName); // .jpg, .png vs.
-                var fileName = $"{DateTime.Now:MMddHHmmss}{extension}";
+                var fileName = $"{DateTime.Now:MMddHHmmss}.{file.FileName.Split(".").Last()}";
                 var fileUrl = Path.Combine(savePath, fileName);
-
                 using (var fileStream = new FileStream(fileUrl, FileMode.Create))
                 {
                     await file.CopyToAsync(fileStream);
                 }
-
                 model.ImagePath = fileName;
                 model.AuthorId = (int)HttpContext.Session.GetInt32("id");
                 await _context.AddAsync(model);
                 await _context.SaveChangesAsync();
-
                 return Json(true);
+
             }
 
             return Json(false);
